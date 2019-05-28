10b7188d135f62e1a6cd3fe98debff2585616562
Ashieqa Dollie <adollie@c3r8s4.wethinkcode.co.za>
2019-05-28 09:51:57 +0200

    created .h

---
+++ get_next_line.h
@@ -0,0 +1,24 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   get_next_line.h                                    :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: adollie <marvin@42.fr>                     +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2019/05/28 09:24:06 by adollie           #+#    #+#             */
+/*   Updated: 2019/05/28 09:50:05 by adollie          ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef GET_NEXT_LINE_H
+# define GET_NEXT_LINE_H
+# include <libft.h>
+# include <stdlib.h>
+# include <unistd.h>
+
+# define BUFF_SIZE 42
+
+# define MALLCHECK(x) if (!x) return (-1);
+
+int		get_next_line(const int fd, char **line);
+#endif
